274910aaa149c2e6f9b1f9032b1f945b903f7779
chinchopachinchopa <mishastik44@ynadex.ru>
2022-11-30 11:04:54 +0300

    Реализовал логику принятия данных и выведения ошибки при неверных данных

--- MyTraining/InPutWindow.xaml.cs
+++ MyTraining/InPutWindow.xaml.cs
@@ -33,10 +33,42 @@ namespace MyTraining
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string Email = email.Text.ToLower();
+            string Password = password.Password;
+
+            if(Password.Length <5)
+            {
+                password.ToolTip = "Некорректный пароль!";
+                password.Background = Brushes.Red;
+                BorderPassword.Background = Brushes.Red;
+            }
+
+            else if ( Email.Length<5 || !Email.Contains("@") || !Email.Contains("."))
+            {
+                email.ToolTip = "Некорректное эл. почта";
+                BorderEmail.Background = Brushes.Red;
+                email.Background = Brushes.Red;
+            }
+            else
+            {
+                password.ToolTip = null;
+                password.Background = Brushes.Transparent;
 
 
-            ExercisesWindow exercisesWindow = new ExercisesWindow();
-            exercisesWindow.ShowDialog();
+                password.ToolTip = null;
+                password.Background = Brushes.Transparent;
+                BorderPassword.Background = Brushes.Transparent;
+
+                email.ToolTip = null;
+                email.Background = Brushes.Transparent;
+                BorderEmail.Background = Brushes.Transparent;
+
+          
+                MessageBox.Show(Email, Password);
+                ExercisesWindow exercisesWindow = new ExercisesWindow();
+                exercisesWindow.ShowDialog();
+            }
+
 
         }
     }
--- MyTraining/RegWindow.xaml.cs
+++ MyTraining/RegWindow.xaml.cs
@@ -27,7 +27,51 @@ namespace MyTraining
 
         private void InPut_Click(object sender, RoutedEventArgs e)
         {
+
+
             NavigationService.Navigate(new InPutWindow());
+        }
+
+        private void Registration_click(object sender, RoutedEventArgs e)
+        {
+            string email = EmailBox.Text;
+            string password = PasswordBox.Password;
+            string rePassword = RePasswordBox.Password;
+
+            if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                EmailBox.ToolTip = "Некорректное эл. почта";
+                BorderEmail.Background = Brushes.Red;
+                EmailBox.Background = Brushes.Red;
+            }
+            if (password!=rePassword)
+            {
+                RePasswordBox.ToolTip = "Неодинаковые пароли!";
+                RePasswordBox.Background = Brushes.Red;
+                BorderRePassword.Background = Brushes.Red;
+                PasswordBox.ToolTip = "Неодинаковые пароли!";
+                PasswordBox.Background = Brushes.Red;
+                BorderPassword.Background = Brushes.Red;
+            }
+            if (password.Length < 5)
+            {
+
+                PasswordBox.ToolTip = "Некорректный пароль!";
+                PasswordBox.Background = Brushes.Red;
+                BorderPassword.Background = Brushes.Red;
+            }
+
+            if (rePassword.Length < 5)
+            {
+                RePasswordBox.ToolTip = "Некорректный пароль!";
+                RePasswordBox.Background = Brushes.Red;
+                BorderRePassword.Background = Brushes.Red;
+            }
+            else
+            {
+                NavigationService.Navigate(new InPutWindow());
+            }
+
         }
     }
 }
